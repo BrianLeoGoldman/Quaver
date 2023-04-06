03cfa59cc6b818007765896de2ddc2480bd54409
Brian Goldman <leonel89011@gmail.com>
2023-04-06 16:46:39 -0300

    Small changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Quaver</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          You have clicked {count} times
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Looking for music? You are in the right place...
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Chill dude, chill...
       </p>
     </div>
   )
