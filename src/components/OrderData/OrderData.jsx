d1907712db5f888ce97a4d0884b2e3f5655c12e1
Brian Goldman <leonel89011@gmail.com>
2023-05-31 23:29:41 -0300

    Completed Profile and Orders views

--- src/components/OrderData/OrderData.jsx
+++ src/components/OrderData/OrderData.jsx
@@ -1,11 +1,11 @@
 import './OrderData.scss'
+import { OrderItemsList } from "../OrderItemsList/OrderItemsList"
 
 export const OrderData = ({ order }) => {
 
     const date = order.date.toDate()
     const finalDate = date.toLocaleString('es-AR')
 
-    console.log()
     return(
         <div className="order">
             <h3 className="id">ID: {order.id}</h3>
@@ -14,7 +14,7 @@ export const OrderData = ({ order }) => {
             <h4 className="data">ADDRESS: {order.client.address}</h4>
             <h4 className="data">DATE: {finalDate}</h4>
             <h4 className="data">TOTAL PAYMENT: ${order.total}</h4>
-            {/* Add purchased items list */}
+            <OrderItemsList items={order.items}/>
         </div>
     )
 }
